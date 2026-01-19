2ffc00d30c4aa87f3da5182aea279c6e25f92b0c
nikiveron <nikiveron@bk.ru>
2026-01-19 22:57:05 +0400

    added knockback to enemy

--- Assets/Scripts/Creatures/Enemy/DealDamageAndKnockback.cs
+++ Assets/Scripts/Creatures/Enemy/DealDamageAndKnockback.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class DealDamageAndKnockback : MonoBehaviour
@@ -5,13 +6,27 @@ public class DealDamageAndKnockback : MonoBehaviour
     [SerializeField] private float _damage = 10f;
     [SerializeField] private float _knockbackForce = 5f;
 
+    private HashSet<GameObject> _damagedObjects = new HashSet<GameObject>();
+
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.TryGetComponent<ObjectHealth>(out var health)) health.TakeDamage(_damage);
+        if (_damagedObjects.Contains(collision.gameObject))
+            return;
+
+        if (collision.gameObject.TryGetComponent<ObjectHealth>(out var health))
+        {
+            _damagedObjects.Add(collision.gameObject);
+            health.TakeDamage(_damage);
+        }
         if (collision.rigidbody)
         {
             Vector3 direction = (collision.transform.position - transform.position).normalized;
             collision.rigidbody.AddForce(direction * _knockbackForce, ForceMode.Impulse);
         }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        _damagedObjects.Remove(collision.gameObject);
     }
 }
